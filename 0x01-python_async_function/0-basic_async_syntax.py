3c503a9533e5f0fd1f033198864a0aac81316353
lumanya <shaibulumanya@gmail.com>
2023-10-14 18:04:43 +0300

    basic coroutine

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" Asynchronous coroutine that takes in an integer argument (max_delay, with
+a default value of 10) named wait_random that waits for a random delay between
+0 and 10 (includes and float) seconds and eventulally returns it.
+"""
+
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """asynchronous couroutine that take an in integer and return a
+    random delay
+    """
+    rand = random.uniform(0, max_delay)
+    return rand
