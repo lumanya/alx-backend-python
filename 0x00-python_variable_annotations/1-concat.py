fd2052990682595d0d904916498d4ee5f750a4ba
lumanya <shaibulumanya@gmail.com>
2023-10-14 11:02:15 +0300

    concat strings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Type-annotated function concat that return contactinationof two strings
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """ return concatinationof strings"""
+    return str1 + str2
