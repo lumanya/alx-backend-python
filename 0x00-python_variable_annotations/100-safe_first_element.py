d436027ac8cc8febb2a5063fe5ca97b2ae459e1d
lumanya <shaibulumanya@gmail.com>
2023-10-14 13:37:31 +0300

    annotate function

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+""" Augment the code """
+import typing
+
+
+def safe_first_element(lst: typing.Sequence[typing.Any]) \
+     -> typing.Union[typing.Any, None]:
+    """ ruturn any data or none """
+    if lst:
+        return lst[0]
+    else:
+        return None
