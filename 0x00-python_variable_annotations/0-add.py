9ba72e1a068ecd931c17e3afa0a3e1f5c1d35752
lumanya <shaibulumanya@gmail.com>
2023-10-14 10:58:05 +0300

    type-annotated function

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Type-annotated function add that takes a float as arguments and returns
+    Float
+"""
+
+
+def add(a: float, b: float) -> float:
+    """ annotated function that return addition of two numbers
+    """
+    return a + b
