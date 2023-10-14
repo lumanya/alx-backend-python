0eff1fb8aea3b816ef41900cef4409305c101a14
lumanya <shaibulumanya@gmail.com>
2023-10-14 13:30:07 +0300

    fix typo on lst

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -3,7 +3,7 @@
 import typing
 
 
-def element_length(list: typing.Iterable[typing.Sequence]) \
+def element_length(lst: typing.Iterable[typing.Sequence]) \
      -> typing.List[typing.Tuple[typing.Sequence, int]]:
     """ annotated function that return list which contains tuple"""
     return [(i, len(i)) for i in lst]
