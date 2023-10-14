bf2855028d5a74e03d6832c588f82c1e5d3e3408
lumanya <shaibulumanya@gmail.com>
2023-10-14 11:16:16 +0300

    type-annotated to_str function

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" type-annotated function to_str that takes float as an argument and return
+str repersentation of float
+"""
+
+
+def to_str(n: float) -> str:
+    """ return str representation of float """
+    return str(n)
