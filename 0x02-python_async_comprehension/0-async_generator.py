15067ffc6c5eaf18438198d21a0238f590fbd15e
lumanya <shaibulumanya@gmail.com>
2023-10-15 22:08:29 +0300

    add type annotation

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """ coroutine that takes no argument
 """
+from typing import Generator
 import asyncio
 import random
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
